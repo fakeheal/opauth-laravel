ec03860325f6385372663d80d29f3b3bf5ad951b
Iv <fakeheal@gmail.com>
2013-01-17 12:50:50 +0200

    Removing unnecessary data.

--- bundles/opauth/start.php
+++ bundles/opauth/start.php
@@ -9,12 +9,12 @@
 	    $config = array(
 			'Strategy' => array(
 				'Facebook' => array(
-					'app_id' 	 => '322542281189355',
-					'app_secret' => '299c474727fe3639db08015cad56ebcb'
+					'app_id' 	 => 'APP_ID',
+					'app_secret' => 'APP_SECRET'
 				),
 				'Twitter' => array(
-					'key' 	 => 'Z72ZGSMAsRrexDqmPKp61A',
-					'secret' => 'iJnhBZuGmmHuvfhwz4b5F34L3OX4VQj00T7eBma76c'
+					'key' 	 => 'APP_KEY',
+					'secret' => 'APP_SECRET'
 				)			
 			),
 	    	'security_salt'	=> 'YOURSALTGOESHERE!',
